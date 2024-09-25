ee0f99e69e0a42e9381d04f179ba4ee6dcedd2f1
Efrain Palencia <epalen6@wgu.edu>
2024-09-24 22:34:01 -0400

    Creates CustomUserManager, Appointment, LabTest, and MedicalRecord models

--- VirtueHealthCore/settings.py
+++ VirtueHealthCore/settings.py
@@ -40,6 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third-party apps
+    'rest_framework',
+
+    # installed apps
+    'user',
 ]
 
 MIDDLEWARE = [
